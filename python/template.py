6e4b826c4b819e4f2a4a74dffd763b97aac3f4a5
tanyuan <tsy754@gmail.com>
2016-02-09 00:46:02 +0800

    Remove templates

--- python/template.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-if __name__ == '__main__' :
-
-    parser = argparse.ArgumentParser(description='Program description')
-    parser.add_argument('input', help='Input file')
-    parser.add_argument('-o', '--option', type=int, help='Integer option')
-    args = parser.parse_args()
-  
