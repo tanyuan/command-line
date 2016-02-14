5c6d985641e37195c3013bb01f51f10a574480ae
tanyuan <tsy754@gmail.com>
2016-02-14 16:52:39 +0800

    Small tweak

--- list-sh.sh
+++ list-sh.sh
@@ -4,10 +4,12 @@
 
 for file in *.sh; do
 
+    # Show file name in bold font
     tput bold
     echo $file
+    # Show comment in normal font
     tput sgr0
+    # Show the third line of the script file, usually a comment
     head -n 3 $file | tail -n 1
-    echo
 
 done
