1583d26226bab2fc897f1e031a38c174efbe9dda
tanyuan <tsy754@gmail.com>
2016-02-14 16:21:59 +0800

    Ignore blank lines

--- list-md.sh
+++ list-md.sh
@@ -4,10 +4,13 @@
 
 for file in *.md; do
 
+    # Show file name in bold font
     tput bold
     echo $file
+    # Show file content in normal font
     tput sgr0
-    head -n 5 $file
+    # Show the first 10 lines and ignore blank lines
+    head  $file | sed -e '/^$/d'
     echo
 
 done
